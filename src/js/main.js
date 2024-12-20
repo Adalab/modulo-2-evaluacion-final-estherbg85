f927b9e1bf5746c2b26a75fea30c5656b9acb3f7
estherbg85 <estherbonillagirol85@gmail.com>
2024-12-20 17:13:47 +0100

    first character in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 
+const charactersUl = document.querySelector('.js_charactersUl');
+
+charactersUl.innerHTML = `
+    <li class="characters_item">
+        <img src="https://static.wikia.nocookie.net/disney/images/d/d3/Vlcsnap-2015-05-06-23h04m15s601.png" alt="imagen de Achilles"/>
+        <h4>Achilles</h4>
+    </li>`;
+
+    
