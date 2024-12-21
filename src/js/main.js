4c8642e1ec65553eb40b6e66cadecb58449893ef
estherbg85 <estherbonillagirol85@gmail.com>
2024-12-21 09:59:51 +0100

    function handleFavorite

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
 "use strict";
+// SECCIÓN QUERY SELECTOR
 
 const charactersUl = document.querySelector(".js_charactersUl");
 
 let allCharacters = [];
 
+// SECCIÓN DE LAS FUNCIONES
+
 const renderOneCharacters = (charactersObj) => {
   const html = `
-        <li class="js_characters_item characters_item favorite">
+        <li class="js_characters_item characters_item">
             <img src="${charactersObj.imageUrl}"/>
             <h4>${charactersObj.name}</h4>
         </li>`;
@@ -21,7 +24,7 @@ const renderAllCharacters = () => {
   }
   charactersUl.innerHTML = html;
 
-  const allCharactersLi = document.querySelector(".js_characters_item");
+  const allCharactersLi = document.querySelectorAll(".js_characters_item");
 
   for (const li of allCharactersLi) {
     li.addEventListener("click", handleFavorite);
@@ -30,12 +33,23 @@ const renderAllCharacters = () => {
 
 const handleFavorite = (ev) => {
   console.log("favoritas");
+  console.log(ev.currentTarget);
+
+  ev.currentTarget.classList.toggle("favorite");
 };
+
+// CUANDO CARGA LA PÁGINA
 
 fetch("https://api.disneyapi.dev/character?pageSize=50")
   .then((response) => response.json())
   .then((data) => {
     allCharacters = data.data;
+    allCharacters.forEach((element) => {
+      if (element.imageUrl === undefined) {
+        element.imageUrl =
+          "https://placehold.co/400x400/ffffff/555555?text=Disney";
+      }
+    });
 
     renderAllCharacters();
   });
